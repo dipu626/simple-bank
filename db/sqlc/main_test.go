bca4a0eeeb8701fe3a582fb72b76b12cef1c93de
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-04-21 09:52:40 +0600

    12 Load config from file & environment variables in Go with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,21 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/dipu626/simple-bank/db/util"
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:6565@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("./../../")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("can't connect to db:", err)
--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/dipu626/simple-bank/api"
 	db "github.com/dipu626/simple-bank/db/sqlc"
+	"github.com/dipu626/simple-bank/db/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:6565@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err = server.Start(serverAddress); err != nil {
+	if err = server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start server:", err)
 	}
 }
